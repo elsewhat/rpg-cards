4e59c5ed57abd72ab2373ff8df27e680230205a1
Dagfinn Parnas <dagfinn.parnas@gmail.com>
2020-09-12 21:06:45 +0200

    Fixed preview in ui.js

--- generator/js/ui.js
+++ generator/js/ui.js
@@ -206,10 +206,52 @@ function ui_render_selected_card() {
     if (card) {
         var front = card_generate_front(card, card_options);
         var back = card_generate_back(card, card_options);
+        
         $('#preview-container').html(front + "\n" + back);
+        card_post_processing();
     }
     local_store_save();
 }
+
+function card_post_processing(){
+    //align this code with card_post_processing.js
+    var counter=0;
+    try {
+      //cleanup old rules
+      for(var i=document.styleSheets[0].rules.length-1 ; i>=0; i--){
+          if(document.styleSheets[0].rules[i].cssText.startsWith(".bg-card")){
+            console.log("Removing rule " + document.styleSheets[0].rules[i].cssText);
+            document.styleSheets[0].removeRule(i);  
+          }
+        }
+      }
+    catch(ex){}
+  
+    $('.card-content-container[data-image]').each(function (index){
+      try {
+        var imageAttr = $(this).attr('data-image');
+        var topAttr=  $(this).attr('data-top');
+        var opacityAttr=  $(this).attr('data-opacity');
+        var className = 'bg-card-'+index;
+        debugger;
+        document.styleSheets[0].addRule('.'+className+':after', 'background-image: url(' +imageAttr+ ')');
+        if(topAttr!= null){
+          document.styleSheets[0].addRule('.'+className+':after', 'background-position-y: ' +topAttr + 'px !important');
+        }
+        if(opacityAttr!= null){
+          document.styleSheets[0].addRule('.'+className+':after', 'opacity: ' +opacityAttr + ' !important');
+        }
+        $(this).addClass(className);
+        counter++;
+      }catch(ex){
+        console.log("Failed to update bgpicture");
+        console.log(ex);
+      }
+    })
+    console.log('Added '  + counter + ' background images to cards');
+    
+  }
+
 
 function ui_open_help() {
     $("#help-modal").modal('show');
